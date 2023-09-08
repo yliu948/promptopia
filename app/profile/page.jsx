af9e8a1f9a97a0bc9f3bf49665dfa6ec770b0e3a
Yi Liu <justinliuyi@gmail.com>
2023-09-08 12:44:47 -0400

    updated edit&delete

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,64 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
 import React from "react";
 
-const Profile = () => {
-  return <div>Profile</div>;
+const MyProfile = () => {
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+
+  const router = useRouter();
+
+  const fetchPosts = async () => {
+    const response = await fetch(`/api/users/${session?.user.id}/posts`);
+    const data = await response.json();
+
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((item) => item._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default MyProfile;
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -8,6 +8,10 @@ import { usePathname, useRouter } from "next/navigation";
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
 
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
@@ -54,8 +58,25 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           handleTagClick && handleTagClick(post.tag);
         }}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
